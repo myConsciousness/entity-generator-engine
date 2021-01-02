bae02498b62b7a4643653c2899839e7d47bae079
Kato Shinya <kato.shinya.dev@gmail.com>
2021-01-02 12:16:01 +0900

    update

--- src/main/java/org/thinkit/generator/entity/engine/factory/EntityCopyright.java
+++ src/main/java/org/thinkit/generator/entity/engine/factory/EntityCopyright.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2021 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package org.thinkit.generator.entity.engine.factory;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
@@ -8,6 +22,12 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+/**
+ * エンティティクラスの著作権を生成する処理を定義したファクトリークラスです。
+ *
+ * @author Kato Shinya
+ * @since 1.0.0
+ */
 @ToString
 @EqualsAndHashCode(callSuper = false)
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
