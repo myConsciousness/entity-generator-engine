fd2719f2b15bf3a206ca298050be56c5e64bbb6c
Kato Shinya <kato.shinya.dev@gmail.com>
2021-01-15 00:02:29 +0900

    update

--- src/test/java/org/thinkit/generator/entity/engine/formatter/EntityResourceFormatterTest.java
+++ src/test/java/org/thinkit/generator/entity/engine/formatter/EntityResourceFormatterTest.java
@@ -36,7 +36,7 @@ import org.thinkit.generator.entity.engine.dto.EntityResourceGroup;
 public final class EntityResourceFormatterTest {
 
         @Test
-        void testSimpleCase() {
+        void testBaseCase() {
 
                 final EntityMeta entityMeta = EntityMeta.builder().version("1.0.0")
                                 .dependentPackages(List.of("java.util.ArrayList", "java.util.List")).build();
@@ -59,8 +59,31 @@ public final class EntityResourceFormatterTest {
                 assertEquals(EXPECTED_ENTITY_DEFAULT, entityResourceGroup.get(0).getResource());
         }
 
+        @Test
+        void testWhenFieldHasDefaultValue() {
+
+                final EntityMeta entityMeta = EntityMeta.builder().version("1.0.0").build();
+
+                final EntityField entityField = EntityField.builder().description("This is the test field.")
+                                .dataType("String").variableName("test").initialValue("test").build();
+
+                final EntityDefinition entityDefinition = EntityDefinition.builder().entityMeta(entityMeta)
+                                .packageName("org.thinkit.test.generator.entity").className("TestEntity")
+                                .entityFields(List.of(entityField)).build();
+
+                final EntityMatrix entityMatrix = EntityMatrix.builder()
+                                .entityCreator(EntityCreator.builder().creator("Kato Shinya").build())
+                                .entityDefinitions(List.of(entityDefinition)).build();
+
+                final EntityResourceGroup entityResourceGroup = EntityResourceFormatter.newInstance()
+                                .format(entityMatrix);
+
+                assertNotNull(entityResourceGroup);
+                assertEquals(EXPECTED_ENTITY_WHEN_FIELD_HAS_INITIAL_VALUE, entityResourceGroup.get(0).getResource());
+        }
+
         /**
-         * エンティティの期待値（デフォルト）
+         * エンティティの期待値
          */
         private static final String EXPECTED_ENTITY_DEFAULT = """
                         /*
@@ -108,6 +131,56 @@ public final class EntityResourceFormatterTest {
 
                             /** This is the test field. */
                             @Getter private String test;
+                        }
+                        """;
+
+        /**
+         * フィールドが初期値を保つ場合のエンティティの期待値
+         */
+        private static final String EXPECTED_ENTITY_WHEN_FIELD_HAS_INITIAL_VALUE = """
+                        /*
+                         * Copyright 2021 Kato Shinya.
+                         *
+                         * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+                         * in compliance with the License. You may obtain a copy of the License at
+                         *
+                         *     http://www.apache.org/licenses/LICENSE-2.0
+                         *
+                         * Unless required by applicable law or agreed to in writing, software distributed under the License
+                         * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+                         * or implied. See the License for the specific language governing permissions and limitations under
+                         * the License.
+                         */
+
+                        package org.thinkit.test.generator.entity;
+
+                        import java.io.Serializable;
+                        import lombok.AccessLevel;
+                        import lombok.AllArgsConstructor;
+                        import lombok.Builder;
+                        import lombok.EqualsAndHashCode;
+                        import lombok.Getter;
+                        import lombok.NoArgsConstructor;
+                        import lombok.ToString;
+
+                        /**
+                         * This entity class was created by Entity Generator.
+                         *
+                         * @author Kato Shinya
+                         * @since 1.0.0
+                         */
+                        @ToString
+                        @EqualsAndHashCode
+                        @Builder(toBuilder = true)
+                        @NoArgsConstructor(access = AccessLevel.PRIVATE)
+                        @AllArgsConstructor(access = AccessLevel.PRIVATE)
+                        public final class TestEntity implements Serializable {
+
+                            /** The serial version UID */
+                            private static final long serialVersionUID = 1L;
+
+                            /** This is the test field. */
+                            @Getter @Builder.Default private String test = "test";
                         }
                         """;
 }
